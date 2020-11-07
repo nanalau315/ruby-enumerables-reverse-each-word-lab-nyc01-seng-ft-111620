e95723d4200b489f9ac298350fcec6591e44e68e
Nana Lau <nanalau315@hotmail.com>
2020-11-07 03:13:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,22 @@
+# def reverse_each_word(string)
+#   arr = string.split(" ")
+#   new_arr = []
+  
+#   arr.each do |word|
+#     new_arr << word.reverse
+#     end
+  
+  
+# new_arr.join(" ")
+
+# end
+
 def reverse_each_word(string)
   arr = string.split(" ")
-  new_arr = []
   
-  arr.each do |word|
-    new_arr << word.reverse
+  
+  arr.collect do |word|
+    word.reverse
     end
   
   
