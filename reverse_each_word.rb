cfb97035f4b138fc3f2d5a39e601ecb6344777cc
Nana Lau <nanalau315@hotmail.com>
2020-11-07 03:17:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,14 +13,14 @@
 
 def reverse_each_word(string)
   arr = string.split(" ")
-  new_arr = []
+  
   
   arr.collect do |word|
-    new_arr = word.reverse
+    puts word.reverse
     
     end
   
-  new_arr.join(" ")
+  
 
 
 end
